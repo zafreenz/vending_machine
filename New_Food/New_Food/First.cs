2a82bba203c90a4be4555b4b5e08bb4028456026
alya <batrisyiaalya13@gmail.com>
2026-06-21 21:16:56 +0800

    Add new First

--- New_Food/New_Food/First.cs
+++ New_Food/New_Food/First.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace New_Food
 {
-    public partial class Form1 : Form
+    public partial class First : Form
     {
-        public Form1()
+        public First()
         {
             InitializeComponent();
         }
